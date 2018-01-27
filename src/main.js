d2056beba930ec8bbe7a25626d1acd68b0ddeb4b
sgl <1135630270@qq.com>
2018-01-27 17:50:57 +0800

    add elem

--- src/main.js
+++ src/main.js
@@ -6,13 +6,15 @@ import router from './router';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 import Components from './components';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false;
 
 // 此处需要use后，this.$http.get或者this.$http.post才可以
 Vue.use(VueRouter);
 Vue.use(VueResource);
-
+Vue.use(ElementUI);
 Vue.use(Components);
 
 /* eslint-disable no-new */
