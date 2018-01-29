02bc0699ccc259d3233c98ccae470c6c04aacda6
grayShi <34054708+grayShi@users.noreply.github.com>
2018-01-29 11:45:54 +0800

    up transaction

--- public/server/baseService.js
+++ public/server/baseService.js
@@ -1,1 +1,21 @@
-module.exports = class BaseService {};
+const seq = require('../utils/connection').get('mssql');
+const { getNamespace } = require('continuation-local-storage');
+
+module.exports = class BaseService {
+  async doTransaction(operations, transaction) {
+    if (typeof transaction === 'undefined' || transaction === null) {
+      return seq.transaction(async t => operations(t));
+    }
+    return operations(transaction);
+  }
+
+  getUserId() {
+    const cls = getNamespace(SESSION_ID);
+    const userId = cls.get(USER_ID);
+    if (!userId) {
+      logger.warn('User Id not found in thread local.');
+      logger.warn('This might caused by transaction created in Resolver/Rejector of Promise');
+    }
+    return userId;
+  }
+};
